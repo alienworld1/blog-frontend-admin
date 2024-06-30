e34b1b939289d60780945d001fc351c61606a17e
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-06-30 20:33:27 +0530

    Add logout route
    -Update routes to validate the user's JSON web token before allowing them to access the dashboard

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,50 @@
 import Pages from './pages';
-import { Navigate } from 'react-router-dom';
+import { redirect } from 'react-router-dom';
+import { deleteToken, validateToken } from './tokenHandler';
+
+const authLoader = async () => {
+  const isValidToken = await validateToken();
+  if (isValidToken) {
+    return redirect('/dashboard');
+  }
+  return redirect('/log-in');
+};
 
 const routes = [
   {
-    index: true,
-    element: <Navigate to="/log-in" />,
+    path: '/',
+    loader: authLoader,
     errorElement: <Pages.Error />,
   },
-  { path: '/log-in', element: <Pages.Login /> },
-  { path: '/dashboard', element: <Pages.Dashboard /> },
+  {
+    path: '/log-in',
+    element: <Pages.Login />,
+    loader: async () => {
+      const isValidToken = await validateToken();
+      if (isValidToken) {
+        return redirect('/dashboard');
+      }
+      return null;
+    },
+  },
+  {
+    path: '/dashboard',
+    element: <Pages.Dashboard />,
+    loader: async () => {
+      const isValidToken = await validateToken();
+      if (!isValidToken) {
+        return redirect('/log-in');
+      }
+      return null;
+    },
+  },
+  {
+    path: '/log-out',
+    loader: () => {
+      deleteToken();
+      return redirect('/log-in');
+    },
+  },
 ];
 
 export default routes;
