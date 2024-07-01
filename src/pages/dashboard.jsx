ab549303f23a30aa94c8ed55533ee57d8aa86171
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-07-01 20:45:03 +0530

    Remove unnecessary token validation check

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,19 +1,7 @@
-import { useEffect } from 'react';
 import Sidebar from '../components/sidebar';
-import { validateToken } from '../tokenHandler';
-import { useNavigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 function Dashboard() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    validateToken().then(isValid => {
-      if (!isValid) {
-        navigate('/log-in');
-      }
-    });
-  }, [navigate]);
-
   return (
     <main className="flex h-full">
       <Sidebar />
